fb4d216c26dca4afa3709f560832cee92b6b69b4
Bruno Ignabai Tchindebe <tchindebebruno@gmail.com>
2024-10-02 17:37:05 +0400

    feat: models des factures

---
+++ Domain/Invoice.cs
@@ -0,0 +1,9 @@
+﻿namespace InvoiceGenerator_CleanArchitecture_Dotnet8.Domain
+{
+    public class Invoice
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public List<InvoiceAttribute>? Attributes { get; set; } 
+    }
+}
---
+++ Domain/InvoiceAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceGenerator_CleanArchitecture_Dotnet8.Domain
+{
+    public class InvoiceAttribute
+    {
+        public string Key { get; set; } = string.Empty;
+        public string Value { get; set; } = string.Empty;
+    }
+}
