bfa4bbb8dd4e7ef3ab07117b3b30242bb97d36dd
Bruno Ignabai Tchindebe <tchindebebruno@gmail.com>
2024-10-02 17:38:03 +0400

    feat: ajout de la methode Main pour lancer l'app

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using InvoiceGenerator_CleanArchitecture_Dotnet8.Application;
+using InvoiceGenerator_CleanArchitecture_Dotnet8.Infrastructure;
+using InvoiceGenerator_CleanArchitecture_Dotnet8.Presentation;
+
+namespace InvoiceGenerator_CleanArchitecture_Dotnet8
+{
+    class Program
+    {
+        static void Main()
+        {
+            string logoPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logo.png");
+
+            var imageSaver = new ImageSaver();
+            var invoiceService = new InvoiceService(imageSaver);
+            var presenter = new InvoicePresenter(invoiceService);
+
+            presenter.CreateInvoice(logoPath);
+        }
+    }
+
+}
