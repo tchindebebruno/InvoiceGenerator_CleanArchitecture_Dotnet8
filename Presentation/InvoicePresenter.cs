68d1729731f39946fb90bd46429856dec9183748
Bruno Ignabai Tchindebe <tchindebebruno@gmail.com>
2024-10-02 17:37:50 +0400

    feat: ajout d'un presenter pour gener la creation d'une facture

---
+++ Presentation/InvoicePresenter.cs
@@ -0,0 +1,26 @@
+﻿using InvoiceGenerator_CleanArchitecture_Dotnet8.Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceGenerator_CleanArchitecture_Dotnet8.Presentation
+{
+    public class InvoicePresenter
+    {
+        private readonly IInvoiceService _invoiceService;
+
+        public InvoicePresenter(IInvoiceService invoiceService)
+        {
+            _invoiceService = invoiceService;
+        }
+
+        public void CreateInvoice(string logoPath)
+        {
+            _invoiceService.GenerateInvoice(logoPath);
+            Console.WriteLine("Facture créée avec succès !");
+        }
+    }
+
+}
